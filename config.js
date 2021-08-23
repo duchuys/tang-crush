682e5f45203907136e0d16e225a643f5e2eab6d0
duchuys <89397647+duchuys@users.noreply.github.com>
2021-08-23 20:46:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
     introTitle: 'Babi à!',
     introDesc: `Em có nhớ hnay là ngày gì ko? kỉ niệm 1000 ngày đóa. Yêu xa nên quà cx 4.0 lun nhá`,
-    btnIntro: 'hihi',
+    btnIntro: 'hihiiiii',
     title: 'Quỳnh có nhớ a hem 😙',
-    desc: 'tloi a xem nao ',
+    desc: 'thử ấn hem xem ',
     btnYes: 'Nhớ bbi nhìu lớmm <33',
     btnNo: 'Hem :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Em nghĩ anh có yêu em mãi mãi ko babi',
     btnReply: 'tloi ngoan thì a thưởng ',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: ' Yêu babi mãi mãi mãi lunnnnn',
     mess: 'Dù có yêu xa thì a vẫn luôn nhớ và yêu quỳnh đó',
-    messDesc: 'Tyêu quỳnh mãi lun.',
-    btnAccept: 'Okiiiii lun <3',
+    messDesc: 'yêu quỳnh mãi lun.',
+    btnAccept: 'Em cũng thíaaaaa',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
